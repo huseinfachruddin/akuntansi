a1888a0e9bc97c962e3532cb8661f66bb4e7b1d1
unknown <husein.18@students.amikom.ac.id>
2021-08-07 09:41:13 +0700

    better

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -47,7 +47,7 @@ class StockController extends Controller
     }
 
     public function getStockTransactionDetail(Request $request){
-        $data = Stocktransaction::where('id',$request->id)->with('contact','cashin','cashout','substocktransaction')->get();
+        $data = Stocktransaction::where('id',$request->id)->with('contact','cashin','cashout','substocktransaction','substocktransaction.product')->get();
         
         $response = [
             'success'=>true,
