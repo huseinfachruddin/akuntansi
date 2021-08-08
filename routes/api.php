17af6d88462cecd7ec401568d0b27377862cf5aa
unknown <husein.18@students.amikom.ac.id>
2021-08-08 14:20:45 +0700

    better

--- routes/api.php
+++ routes/api.php
@@ -30,9 +30,7 @@ use App\Http\Controllers\StockController;
 */
 
 Route::get('/setup/awal',function(Request $request){
-    $role = new Role;
-    $role->name='admin';
-    $role->save();
+ 
 
     $role = new Akun;
     $role->name='admin';
