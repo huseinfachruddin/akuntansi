b2444315b914a7df321f3c206480a313c208ebf5
unknown <husein.18@students.amikom.ac.id>
2021-10-18 19:29:29 +0700

    husin

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -76,7 +76,7 @@ class StockController extends Controller
     }
 
     public function getStockOut(Request $request){
-        $data = Stocktransaction::whereNotNull('cashin_id')->where('pending',false)->orWhere('pending',null);
+        $data = Stocktransaction::whereNotNull('cashin_id');
         if (!empty($request->start_date) && !empty($request->end_date)) {
             $data = $data->whereBetween('date',[$request->start_date,$request->end_date]);
         }else{
