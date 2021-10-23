e47a6dd63f21288fc28163a4242207346b699535
unknown <husein.18@students.amikom.ac.id>
2021-10-24 02:08:49 +0700

    husin

--- app/Http/Controllers/ReportNeracaController.php
+++ app/Http/Controllers/ReportNeracaController.php
@@ -26,7 +26,7 @@ class ReportNeracaController extends Controller
                 }else{
                     $stock = $stock->whereBetween('date',[date('0000-01-01',time()),date('Y-m-d',time())]);
                 }
-                $stock = $stock->whereNotNull('cashin_id');
+                $stock = $stock->whereNotNull('cashin_id')->where('pending','<>',0);
             })->select(DB::raw("SUM(total)"));
         },
         // CREDIT STOCK KELUAR = menghitung uang keluar dari stock
@@ -39,7 +39,7 @@ class ReportNeracaController extends Controller
                 }else{
                     $stock = $stock->whereBetween('date',[date('0000-01-01',time()),date('Y-m-d',time())]);
                 }
-                $stock = $stock->whereNotNull('cashout_id');
+                $stock = $stock->whereNotNull('cashout_id')->where('pending','<>',0);
             })->select(DB::raw("SUM(total)"));    
         },
         // CASH FROM = menghitung cash sebagai akun
