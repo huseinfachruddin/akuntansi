1fc018f5964241a5408910ae86c0010bd9577a33
unknown <husein.18@students.amikom.ac.id>
2021-10-24 22:39:44 +0700

    husin

--- app/Http/Controllers/CashController.php
+++ app/Http/Controllers/CashController.php
@@ -222,13 +222,12 @@ class CashController extends Controller
             $sub->cashtransaction_id = $cash->id;
             $sub->akun_id = $request->akun_id[$key];
             $sub->total = $request->total[$key];
-            $akun = Akun::where('name','Modal')->with(str_repeat('children.',10))->get();
+            $akun = Akun::where('name','Kewajiban')->where('name','Modal')->with(str_repeat('children.',10))->get();
             $nama = Akun::find($sub->akun_id);
 
             $cek=rekursif($akun,$sub,$nama);
-            if ($cek==true) {
-                $sub->total=-1;
-            }
+           
+            $sub->total=-1*$sub->total;
             $sub->desc = null;
             $sub->save();
 
