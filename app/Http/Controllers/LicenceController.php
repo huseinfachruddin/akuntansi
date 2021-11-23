bcd763d88fd8231b5ff5affff334a0899cd6f34a
unknown <husein.18@students.amikom.ac.id>
2021-11-23 22:04:18 +0700

    husein

--- app/Http/Controllers/LicenceController.php
+++ app/Http/Controllers/LicenceController.php
@@ -27,8 +27,7 @@ class LicenceController extends Controller
             'company' =>'required',
             'address'  =>'required',
         ]);
-        $data = Licence::whereNotNull('id')->get()->delete();
-        
+                
         $data = new Licence;
         $data->licence = $request->licence;
         $data->company = base64_encode($request->company);
