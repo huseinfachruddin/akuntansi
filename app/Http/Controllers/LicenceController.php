3a48fc1338d17edd27d04c2167a19ff2f9fd6fab
unknown <husein.18@students.amikom.ac.id>
2021-11-25 10:55:16 +0700

    husein

--- app/Http/Controllers/LicenceController.php
+++ app/Http/Controllers/LicenceController.php
@@ -9,6 +9,7 @@ class LicenceController extends Controller
 {
     public function getLicence(Request $request){
         $data = Licence::first();
+        dd(base64_decode($data->company,true) );
         if ($data) {
             if (base64_decode($data->company,true) && base64_decode($data->address,true)) {
                 $data->company = base64_decode($data->company);
